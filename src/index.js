a2f6c96cc929150e420a3c0edf7f42a0adf8ff6a
Celine Wang <wang_nannan@outlook.com>
2024-11-13 00:17:11 -0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -7,16 +7,34 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Meals from './Components/Meals';
 import Planner from './Components/Planner';
+import WelcomePage from './pages/WelcomePage'
+import InvestPage11 from './pages/InvestPage11';
+import InvestLayout from './layouts/InvestLayout';
+import InvestPage12 from './pages/InvestPage12';
+import InvestPage21 from './pages/InvestPage21';
+import InvestPage22 from './pages/InvestPage22';
+import InvestPage31 from './pages/InvestPage31';
+import InvestPage13 from './pages/InvestPage13';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App></App>}>
-          <Route path='/home' element={<Home></Home>}></Route>
-          <Route path='/meals' element={<Meals></Meals>}></Route>
-          <Route path='/planner' element={<Planner></Planner>}></Route>
+    <Routes>
+        {/* Set the default route to WelcomePage */}
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/invest" element={<InvestLayout />}>
+          <Route path="/invest/1-1" element={<InvestPage11 />} />
+          <Route path="/invest/1-2" element={<InvestPage12 />} />
+          <Route path="/invest/1-3" element={<InvestPage13 />} />
+          <Route path="/invest/2-1" element={<InvestPage21 />} />
+          <Route path="/invest/2-2" element={<InvestPage22 />} />
+          <Route path="/invest/3-1" element={<InvestPage31 />} />
+        </Route>
+        <Route path='/app' element={<App />}>
+          <Route path='/app/home' element={<Home />} />
+          <Route path='/app/meals' element={<Meals />} />
+          <Route path='/app/planner' element={<Planner />} />
         </Route>
       </Routes>
     </BrowserRouter>
